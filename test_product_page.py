a9bee5162f62ff766be7cb0e0cfe900cf0f0c4c5
Maksim Klyandin <maksim.klyandin@gmail.com>
2025-06-03 00:34:26 +0800

    test: update test link for exercise 4.3.3
    Replace the test URL for exercise 4.3.2 with the one for exercise 4.3.3 to test a different product page scenario.

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,8 @@
 from pages.product_page import ProductPage
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear" #задание 4.3.2
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019" #задание 4.3.3
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
